a130f003221ec63567763043a7f2edf741ef8437
chadsteele <chad@chadsteele.com>
2024-06-18 21:34:56 +0400

    flip and tossing cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { createCards } from "./cards"
-import { For } from "solid-js"
+import { For, createSignal, createEffect } from "solid-js"
 
-function App() {
+function App () {
   return (
     <>
       <Dealer />
@@ -10,7 +10,7 @@ function App() {
   )
 }
 
-function Dealer() {
+function Dealer () {
   return (
     <>
       <div>
@@ -21,7 +21,7 @@ function Dealer() {
   )
 }
 
-function Player() {
+function Player () {
   return (
     <>
       <div>
@@ -32,19 +32,34 @@ function Player() {
   )
 }
 
-function Deck() {
+function Deck () {
   const cards = createCards()
   return (
     <>
-      <div>I'm a Deck! {cards[0]}</div>
+
       <div class="stack">
-        <For each={cards}>{(text) => <Card text={text} />}</For>
+        <For each={["", "", ""]}>{(_, i) => {
+          const left = i() * 3 + "px"
+          const top = i() * 3 + "px"
+          return <Card down={true} style={{ top, left }} />
+        }
+        }</For>
+      </div>
+
+      <div class="stack">
+        <For each={cards}>{(text) => {
+          const top = Math.random() * 30 + "vh"
+          const left = Math.random() * 70 + "vw"
+          const rotate = -33 + Math.random() * 66 + "deg"
+          return <Card text={text} down={Math.round(Math.random() - .3)} style={{ top, left, rotate }} />
+        }
+        }</For>
       </div>
     </>
   )
 }
 
-function Hand() {
+function Hand () {
   return (
     <>
       <div>I'm a Hand!</div>
@@ -52,11 +67,43 @@ function Hand() {
   )
 }
 
-function Card(props) {
+let zIndex = 1
+
+function Card (props) {
+
+  const [down, setDown] = createSignal(props.down)
+  const [toss, setToss] = createSignal(true)
+  const [z, setZ] = createSignal(1)
+  const [style, setStyle] = createSignal("")
+
+  // const faces = "♠♥♣♦"
+  const text = props.text?.trim() || ""
+  const value = text.slice(0, text.length - 1)
+  const suit = text.slice(-1)
+  const color = "♥♦".includes(suit) ? "red" : "black"
+
+  createEffect(() => {
+    setStyle({ ...props.style, color, "z-index": z() })
+  })
+
+
   return (
-    <>
-      <div class="card">{props.text}</div>
-    </>
+    <div onClick={() => {
+      setDown(!down())
+      setToss(false)
+      setZ(++zIndex)
+    }}>
+      <div class={`card ${toss() && "toss"} ${down() && "down"}`} style={style()}>
+        <div class="content">
+          <div class="front">
+            <div class="value">{value}</div>
+            <div class="suit">{suit}</div>
+          </div>
+          <div class="back down"></div>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
