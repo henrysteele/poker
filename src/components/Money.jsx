0013dcb98d3634347d25983edf3d3449e2eecf82
henrysteele <henrysogod@gmail.com>
2024-07-07 11:40:48 +0400

    fixed oncall bug and disabled slider

--- src/components/Money.jsx
+++ src/components/Money.jsx
@@ -1,13 +1,13 @@
 import { For, createSignal, createEffect, onMount } from "solid-js"
 import {
-    Box,
-    Card,
-    Stack,
-    CardContent,
-    CardActions,
-    Container,
-    Button,
-    ButtonGroup,
+  Box,
+  Card,
+  Stack,
+  CardContent,
+  CardActions,
+  Container,
+  Button,
+  ButtonGroup,
 } from "@suid/material"
 import "./Money.css"
 import { pot, setPot, placeBet } from "./DrPokerGame"
@@ -15,109 +15,114 @@ import $ from "jquery"
 import config from "./config"
 import { tossCoins } from "./animations.jquery"
 
-export function Money (props) {
-    const [output, setOutput] = createSignal([])
-    const [total, setTotal] = createSignal(props.total)
-    const [bet, setBet] = createSignal(5)
-    const [call, setCall] = createSignal(props.call)
+export function Money(props) {
+  const [output, setOutput] = createSignal([])
+  const [total, setTotal] = createSignal(props.total)
+  const [bet, setBet] = createSignal(5)
+  const [call, setCall] = createSignal(props.call)
+  const [showSlider, setShowSlider] = createSignal()
 
-    createEffect(() => {
-        setTotal(props.total || 0)
+  createEffect(() => {
+    setTotal(props.total || 0)
+  })
+
+  createEffect(() => {
+    setCall(props.call || 0)
+  })
+
+  createEffect(() => {
+    setShowSlider(!(total() < 2 || call() > 0))
+  })
+
+  createEffect(() => {
+    let value = Math.round(total())
+    let out = []
+
+    const denominations = [5000, 1000, 50, 15, 6, 2, 1]
+    denominations.forEach((denomination) => {
+      const label = "coin coin" + denomination
+      const count = Math.floor(value / denomination)
+
+      for (let i = 0; i < Math.min(count, 3); i++) {
+        const bottom = Math.random() * 2 + "%"
+        const left = 50 - Math.random() * 20 + "%"
+        const mirror = Math.random() > 0.5 ? "mirror" : ""
+        const src = `/dist/coins/${denomination}.png`
+
+        out.push(
+          <div class={label} style={{ bottom, left }}>
+            <img class={mirror} src={src} />
+          </div>
+        )
+      }
+      value = value - count * denomination
     })
+    setOutput(out)
+  })
 
-    createEffect(() => {
-        setCall(props.call || 0)
+  function onSlide(e) {
+    setBet(Math.round(e.target.value)) // ensure it's a number, not a string!
+  }
+
+  function onBet(amount = 0) {
+    amount = Math.max(0, Math.min(amount, total()))
+    tossCoins(amount, `#player-${props.name} .money`, "#dealer .money", () => {
+      setPot(pot() + amount)
+      placeBet(props.name, amount)
+      setBet(Math.min(amount, Math.round(total() / 2)))
     })
+  }
 
-    createEffect(() => {
-        let value = Math.round(total())
-        let out = []
-
-        const denominations = [5000, 1000, 50, 15, 6, 2, 1]
-        denominations.forEach((denomination) => {
-            const label = "coin coin" + denomination
-            const count = Math.floor(value / denomination)
-
-            for (let i = 0; i < Math.min(count, 3); i++) {
-                const bottom = Math.random() * 2 + "%"
-                const left = 50 - Math.random() * 20 + "%"
-                const mirror = Math.random() > 0.5 ? "mirror" : ""
-                const src = `/dist/coins/${denomination}.png`
-
-                out.push(
-                    <div class={label} style={{ bottom, left }}>
-                        <img class={mirror} src={src} />
-                    </div>
-                )
-            }
-            value = value - count * denomination
-        })
-        setOutput(out)
+  function onCall() {
+    if (call() == 0) return
+    const amount = call()
+    setCall(0)
+    tossCoins(amount, `#player-${props.name} .money`, "#dealer .money", () => {
+      setPot(pot() + amount)
+      placeBet(props.name, amount)
     })
+  }
 
-    function onSlide (e) {
-        setBet(Math.round(e.target.value)) // ensure it's a number, not a string!
-    }
+  return (
+    <Box>
+      {/* dollar amount */}
+      <div style={{ "text-align": "center", "font-size": "larger" }}>
+        <span class="capitalize">{props.name || "Total "}</span>
+        {props.name ? " has " : ""}$
+        {new Intl.NumberFormat("en-US").format(Math.round(total()))}
+      </div>
+      <Box class="money"> {output()} </Box>
 
-    //todo:  when all 3 bet $1000 the pot is more than $3000
-
-    function onBet (amount = 0) {
-        amount = Math.max(0, Math.min(amount, total()))
-        tossCoins(amount, `#player-${props.name} .money`, "#dealer .money", () => {
-            setPot(pot() + amount)
-            placeBet(props.name, amount)
-            setBet(Math.min(amount, Math.round(total() / 2)))
-        })
-    }
-
-    function onCall () {
-        const amount = call()
-        tossCoins(amount, `#player-${props.name} .money`, "#dealer .money", () => {
-            setPot(pot() + amount)
-            placeBet(props.name, amount)
-        })
-    }
-
-    return (
+      <Show when={props.controls ?? true}>
         <Box>
-            {/* dollar amount */}
-            <div style={{ "text-align": "center", "font-size": "larger" }}>
-                <span class="capitalize">{props.name || "Total "}</span>
-                {props.name ? " has " : ""}$
-                {new Intl.NumberFormat("en-US").format(Math.round(total()))}
-            </div>
-            <Box class="money"> {output()} </Box>
+          <Button variant="x" disabled={call() == 0} onClick={onCall}>
+            call {call() ? "$" + call() : ""} 😘
+          </Button>
 
+          <Button
+            variant="x"
+            onClick={() => onBet(bet())}
+            disabled={call() > 0 || total() == 0}
+          >
+            bet ${bet()} 💰
+          </Button>
 
-            <Show when={props.controls ?? true}>
-                <Box>
+          <Button variant="x">fold 💩</Button>
 
-                    <Button variant="x" onClick={onCall} disabled={call() == 0}>
-                        call {call() ? "$" + call() : ""} 😘
-                    </Button>
-
-
-                    <Button variant="x" onClick={() => onBet(bet())} disabled={call() > 0 || total() == 0}>
-                        bet ${bet()} 💰
-                    </Button>
-
-                    <Button variant="x">fold 💩</Button>
-
-                    {/* slider */}
-                    <div class="slidecontainer">
-                        <input
-                            type="range"
-                            min="1"
-                            max={Math.min(100, total())}
-                            value={bet()}
-                            class="slider"
-                            onInput={onSlide}
-                            disabled={total() < 2}
-                        />
-                    </div>
-                </Box>
-            </Show>
-
+          {/* slider */}
+          <div class={"slidecontainer " + (!showSlider() && "hideSlider")}>
+            <input
+              type="range"
+              min="1"
+              max={Math.min(100, total())}
+              value={bet()}
+              class="slider"
+              onInput={onSlide}
+              disabled={!showSlider()}
+            />
+          </div>
         </Box>
-    )
+      </Show>
+    </Box>
+  )
 }
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,84 +1,94 @@
 import { For, createSignal, createEffect, onMount, children } from "solid-js"
 import {
-    Box,
-    Card,
-    Stack,
-    CardContent,
-    CardActions,
-    Container,
-    Button,
+  Box,
+  Card,
+  Stack,
+  CardContent,
+  CardActions,
+  Container,
+  Button,
 } from "@suid/material"
 
 import { Money } from "./Money"
 import config from "./config"
-import { setShowingCards, players, setPlayers, pot, hands, bets, wallets } from "./DrPokerGame"
+import {
+  setShowingCards,
+  players,
+  setPlayers,
+  pot,
+  hands,
+  bets,
+  wallets,
+} from "./DrPokerGame"
 
-export function Profile (props) {
-    return (
-        <div>
-            <div
-                style={{
-                    ...config.style.profile.image,
-                    ...props.style,
-                    background: `url(${props.src})`,
-                }}
-            ></div>
-        </div>
-    )
+export function Profile(props) {
+  return (
+    <div>
+      <div
+        style={{
+          ...config.style.profile.image,
+          ...props.style,
+          background: `url(${props.src})`,
+        }}
+      ></div>
+    </div>
+  )
 }
 
-export function Dealer (props) {
-    return (
-        <Box id={`dealer`} sx={{ margin: "1em", display: "inline-block" }}>
-            <Card sx={{ padding: "1em", width: "fit-content" }}>
-                <CardContent>
-                    <Stack direction="row">
-                        <Profile name="dealer" src="/dist/peeps/dealer.png" />
-                        <Money total={props.total} controls={false} />
-                    </Stack>
-                </CardContent>
-                <CardActions>{props.children} </CardActions>
-            </Card>
-        </Box>
-    )
+export function Dealer(props) {
+  return (
+    <Box id={`dealer`} sx={{ margin: "1em", display: "inline-block" }}>
+      <Card sx={{ padding: "1em", width: "fit-content" }}>
+        <CardContent>
+          <Stack direction="row">
+            <Profile name="dealer" src="/dist/peeps/dealer.png" />
+            <Money total={props.total} controls={false} />
+          </Stack>
+        </CardContent>
+        <CardActions>{props.children} </CardActions>
+      </Card>
+    </Box>
+  )
 }
 
-export function Player (props) {
-    const [call, setCall] = createSignal(0)
+export function Player(props) {
+  const [call, setCall] = createSignal(0)
 
-    createEffect(() => {
-        let maxBet = Object.values(bets()).reduce((acc, cur) => Math.max(acc, cur), 0)
-        setCall(maxBet - bets()[props.name])
-    })
-
-
-    function onTada () {
-        const cards = Object.values(hands()).flat(Infinity)
-        setShowingCards(cards)
-    }
-
-    return (
-        <Box
-            id={`player-${props.name}`}
-            sx={{ display: "inline-block", margin: "1em" }}
-        >
-            <Card sx={{ width: "fit-content" }}>
-                <CardContent>
-                    <Stack direction="row">
-                        <Stack direction="column">
-                            <Profile name={props.name} src={props.src} />
-                            <Button variant="xcontained" onClick={onTada}>
-                                💥 tada!
-                            </Button>
-                        </Stack>
-                        <Stack direction="column">
-                            <Money total={props.total} call={call()} name={props.name} />
-                            <Box sx={{ margin: "1em" }}>{children(props.children)}</Box>
-                        </Stack>
-                    </Stack>
-                </CardContent>
-                <CardActions></CardActions>
-            </Card>
-        </Box>
+  createEffect(() => {
+    let maxBet = Object.values(bets()).reduce(
+      (acc, cur) => Math.max(acc, cur),
+      0
     )
+    setCall(maxBet - bets()[props.name])
+  })
+
+  function onTada() {
+    const cards = Object.values(hands()).flat(Infinity)
+    setShowingCards(cards)
+  }
+
+  return (
+    <Box
+      id={`player-${props.name}`}
+      sx={{ display: "inline-block", margin: "1em" }}
+    >
+      <Card sx={{ width: "fit-content" }}>
+        <CardContent>
+          <Stack direction="row">
+            <Stack direction="column">
+              <Profile name={props.name} src={props.src} />
+              <Button variant="xcontained" onClick={onTada}>
+                💥 tada!
+              </Button>
+            </Stack>
+            <Stack direction="column">
+              <Money total={props.total} call={call()} name={props.name} />
+              <Box sx={{ margin: "1em" }}>{children(props.children)}</Box>
+            </Stack>
+          </Stack>
+        </CardContent>
+        <CardActions></CardActions>
+      </Card>
+    </Box>
+  )
 }
