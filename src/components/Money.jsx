c470b1824104030f76c719008d0421c2cffc12f3
chadsteele <chad@chadsteele.com>
2024-07-11 21:31:01 +0400

    added setBusy() to ensure bets don't overlap and overrun

--- src/components/Money.jsx
+++ src/components/Money.jsx
@@ -15,12 +15,13 @@ import $ from "jquery"
 import config from "./config"
 import { tossCoins } from "./animations.jquery"
 
-export function Money(props) {
+export function Money (props) {
   const [output, setOutput] = createSignal([])
   const [total, setTotal] = createSignal(props.total)
   const [bet, setBet] = createSignal(5)
   const [call, setCall] = createSignal(props.call)
   const [showSlider, setShowSlider] = createSignal()
+  const [busy, setBusy] = createSignal(false)
 
   createEffect(() => {
     setTotal(props.total || 0)
@@ -60,26 +61,30 @@ export function Money(props) {
     setOutput(out)
   })
 
-  function onSlide(e) {
+  function onSlide (e) {
     setBet(Math.round(e.target.value)) // ensure it's a number, not a string!
   }
 
-  function onBet(amount = 0) {
+  function onBet (amount = 0) {
+    setBusy(true)
     amount = Math.max(0, Math.min(amount, total()))
     tossCoins(amount, `#player-${props.name} .money`, "#dealer .money", () => {
       setPot(pot() + amount)
       placeBet(props.name, amount)
       setBet(Math.min(amount, Math.round(total() / 2)))
+      setBusy(false)
     })
   }
 
-  function onCall() {
+  function onCall () {
     if (call() == 0) return
+    setBusy(true)
     const amount = call()
     setCall(0)
     tossCoins(amount, `#player-${props.name} .money`, "#dealer .money", () => {
       setPot(pot() + amount)
       placeBet(props.name, amount)
+      setBusy(false)
     })
   }
 
@@ -95,14 +100,14 @@ export function Money(props) {
 
       <Show when={props.controls ?? true}>
         <Box>
-          <Button variant="x" disabled={call() == 0} onClick={onCall}>
+          <Button variant="x" disabled={busy() || call() == 0} onClick={onCall}>
             call {call() ? "$" + call() : ""} 😘
           </Button>
 
           <Button
             variant="x"
             onClick={() => onBet(bet())}
-            disabled={call() > 0 || total() == 0}
+            disabled={busy() || call() > 0 || total() == 0}
           >
             bet ${bet()} 💰
           </Button>
