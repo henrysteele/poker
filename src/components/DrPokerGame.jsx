a6926316b35f6f76969d08dd18f8e6e8439e48f5
chadsteele <chad@chadsteele.com>
2024-07-20 14:17:14 +0400

    improved init

--- src/components/DrPokerGame.jsx
+++ src/components/DrPokerGame.jsx
@@ -31,7 +31,7 @@ export const [status, setStatus] = createSignal("Click the deck to deal")
 
 export const [showingCards, setShowingCards] = createSignal([]) // [id]
 
-export function topCard(cards = deck()) {
+export function topCard (cards = deck()) {
   return cards.slice(-1)[0]
 }
 
@@ -43,14 +43,14 @@ const accessors = [
   [grid, setGrid],
 ]
 
-function replaceCards(a, b) {
+function replaceCards (a, b) {
   accessors.forEach((access) => {
     const json = JSON.stringify(access[0]())
     access[1](JSON.parse(json.replace(a, b)))
   })
 }
 
-function swapCards(a, b) {
+function swapCards (a, b) {
   const temp = a.split("").join("**")
   let all = JSON.stringify(accessors.map((access) => access[0]()))
   all = all.replace(a, temp)
@@ -63,7 +63,7 @@ function swapCards(a, b) {
   }
 }
 
-function tossCards(list, callback, i = 0) {
+function tossCards (list, callback, i = 0) {
   // list can have 2 or more cards, cards will be tossed forward
 
   const len = list.length
@@ -89,11 +89,11 @@ function tossCards(list, callback, i = 0) {
   }
 }
 
-function delay(time) {
+function delay (time) {
   return new Promise((resolve) => setTimeout(resolve, time))
 }
 
-export function placeBet(name, amount) {
+export function placeBet (name, amount) {
   let tmp = structuredClone(bets())
   tmp[name] += amount
   setBets(tmp)
@@ -102,8 +102,8 @@ export function placeBet(name, amount) {
   setWallets(tmp)
 }
 
-export function DrPokerGame(props) {
-  function reset() {
+export function DrPokerGame (props) {
+  function reset () {
     const names = players().map((player) => player.name)
     setSelectedIds([])
     setShowingCards([])
@@ -114,18 +114,15 @@ export function DrPokerGame(props) {
     setActivePlayer(names[0])
   }
 
-  function init(names) {
-    const avatars = config.profilenames
-    names = names || props.names || []
-    setPlayers([
-      ...names.map((name, i) => {
-        const src = `/dist/peeps/${avatars[i]}.png`
-        return { name, src }
-      }),
-      { name: user().username, src: user().imageUrl },
+  function init (names) {
+
+    const list = user().unknown ? [] : [
+      { name: user().username, src: user().imageUrl, },
       { name: "Botman", src: "/dist/peeps/batman.png", bot: true },
-    ])
-    names = players().map((player) => player.name)
+    ]
+
+    setPlayers(list)
+    names = players().map(player => player.name)
     setWallets(createMap(names, config.freemoney || 1000))
     setBets(createMap(names, 0))
     reset()
@@ -138,13 +135,13 @@ export function DrPokerGame(props) {
     init()
   })
 
-  function addToWallet(name, amount) {
+  function addToWallet (name, amount) {
     const tmp = structuredClone(wallets())
     tmp[name] += amount
     setWallets(tmp)
   }
 
-  function onDeal() {
+  function onDeal () {
     reset()
 
     const cards = structuredClone(deck())
@@ -236,7 +233,7 @@ export function DrPokerGame(props) {
     }
   })
 
-  function nextPlayer() {
+  function nextPlayer () {
     // next player
     const names = players().map((player) => player.name)
     let i = names.indexOf(activePlayer()) + 1
@@ -246,7 +243,7 @@ export function DrPokerGame(props) {
     setStatus(`It's ${activePlayer()}'s turn`)
   }
 
-  function bothAreInHand(ids) {
+  function bothAreInHand (ids) {
     let count = 0
     hands()[activePlayer()].forEach((card) => {
       ids.forEach((id) => {
@@ -298,7 +295,7 @@ export function DrPokerGame(props) {
     }
   })
 
-  function autoBot() {
+  function autoBot () {
     const pause = 3000
     const me = players().find((player) => player.name == activePlayer())
     if (me?.bot) {
